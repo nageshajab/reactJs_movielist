c7e6bb7ceefc93fc175d289d98c5a96717ebf3a5
Nagesh Ajab <nageshajab@hotmail.com>
2025-08-10 16:28:47 +0530

    11

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,16 @@
 
 //for local development
-export const API_URL = " http://localhost:7018/api";
-export const redirectUri= "http://localhost:5173"
-export const CLIENT_ID ="0daba125-36a6-44e7-a97a-bf4fe79aa57a";
-export const TENANT_ID ="51962fd4-2ece-4606-bdbe-cc684c94f8b0"
+// export const API_URL = " http://localhost:7018/api";
+// export const redirectUri= "http://localhost:5173"
+// export const CLIENT_ID ="0daba125-36a6-44e7-a97a-bf4fe79aa57a";
+// export const TENANT_ID ="51962fd4-2ece-4606-bdbe-cc684c94f8b0"
 
 //for production deployment
-//  export const API_URL = "https://myreactappapimanagementservice.azure-api.net/myreactappbackendapi"
-//  export const redirectUri= "https://mango-dune-01a4d7d10.2.azurestaticapps.net"
-export const subscription_key= "feb18fe559a248ccaa1ff22b56aaa3f2";
-//  export const CLIENT_ID ="68e52705-016a-4070-9a05-2374c876a71d";
-//  export const TENANT_ID ="51962fd4-2ece-4606-bdbe-cc684c94f8b0"
+ export const API_URL = "https://myreactappapimanagementservice.azure-api.net/myreactappbackendapi"
+ export const redirectUri= "https://white-water-0f898f810.2.azurestaticapps.net"
+ export const CLIENT_ID ="c6e448fa-21fe-42e7-afa3-c6062c8219a0";
+ export const TENANT_ID ="51962fd4-2ece-4606-bdbe-cc684c94f8b0"
 
+
+ export const subscription_key= "feb18fe559a248ccaa1ff22b56aaa3f2";
 
